288ce98fce595cd804bf29a4339c0e6a2c49cd19
Khalid Safy <khalid_safy88@hotmail.com>
2021-09-29 00:23:58 +0100

    added LocationDetails component and PropTypes validation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { location } from "./data/forecast.json";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App location={location} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-

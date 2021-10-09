300d29c9cfd56c39550508c775096a347e70957d
Khalid Safy <khalid_safy88@hotmail.com>
2021-10-09 19:20:18 +0100

    Set up WeatherIcon

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
+import WeatherIcon from "react-icons-weather";
 
 const ForecastSummary = (props) => {
   const { date, temperature, description, icon } = props;
-
   return (
     <div className="forecast-summary" data-testid="forecast-summary">
       <div className="forecast-summary_date">{date}</div>
@@ -14,6 +14,7 @@ const ForecastSummary = (props) => {
       <div className="forecast-summary_icon" data-testid="forecast-icon">
         {icon}
       </div>
+      <WeatherIcon name="owm" iconId={icon} />
     </div>
   );
 };

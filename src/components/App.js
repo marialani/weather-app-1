e9f1186e77879bcefad599c683687426b1079e5f
Khalid Safy <khalid_safy88@hotmail.com>
2021-10-10 21:45:17 +0100

    Implemented setup for LocationDetails in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
 import "../styles/App.css";
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 
 const App = ({ location, forecasts }) => {
+  const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
+
+  const selectedForecast = forecasts.filter(
+    (forecast) => forecast.date === selectedDate
+  );
+
   const { city, country } = location;
+
   return (
     <div className="weather-app">
       <h1>Weather App</h1>
       <div className="forecast">
         <LocationDetails city={city} country={country} />
         <ForecastSummaries forecasts={forecasts} />
-        <ForecastDetails forecast={forecasts[0]} />
+        <ForecastDetails forecast={selectedForecast} />
       </div>
     </div>
   );

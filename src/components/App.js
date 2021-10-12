fd1e02697abf74b0641e508616ec28b9f3b749be
Khalid Safy <khalid_safy88@hotmail.com>
2021-10-12 20:53:38 +0100

    Cleaned up App

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import "../styles/App.css";
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import getForecast from "../requests/getForecast";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
@@ -19,18 +19,9 @@ const App = () => {
   const handleForecastSelect = (date) => {
     setSelectedDate(date);
   };
-  const getForecast = () => {
-    const endpoint = "https://mcr-codes-weather-app.herokuapp.com/forecast";
-
-    axios.get(endpoint).then((response) => {
-      setSelectedDate(response.data.forecasts[0].date);
-      setForecasts(response.data.forecasts);
-      setLocation(response.data.location);
-    });
-  };
 
   useEffect(() => {
-    getForecast();
+    getForecast(setSelectedDate, setForecasts, setLocation);
   }, []);
 
   const { city, country } = location;
--- src/components/ForecastSummaries.js
+++ src/components/ForecastSummaries.js
@@ -10,7 +10,7 @@ const ForecastSummaries = ({ forecasts, onForecastSelect }) => (
         key={forecast.date}
         date={forecast.date}
         description={forecast.description}
-        icon={forecast.icon}
+        icon={forecast.icon.toString()}
         temperature={forecast.temperature}
         onSelect={onForecastSelect}
       />
@@ -24,7 +24,7 @@ ForecastSummaries.propTypes = {
     PropTypes.shape({
       date: PropTypes.number.isRequired,
       description: PropTypes.string.isRequired,
-      icon: PropTypes.string.isRequired,
+      icon: PropTypes.number.isRequired,
       temperature: PropTypes.shape({
         min: PropTypes.number,
         max: PropTypes.number,

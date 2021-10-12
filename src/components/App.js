d55a019a561a93e66f830c4fc29d5503b86a8110
Khalid Safy <khalid_safy88@hotmail.com>
2021-10-12 19:08:53 +0100

    Began refactoring for API calls, removed previous static data

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import "../styles/App.css";
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 
-const App = ({ location, forecasts }) => {
-  const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
+const App = () => {
+  const [location, setLocation] = useState({ city: "", country: "" });
+
+  const [forecasts, setForecasts] = useState([]);
+
+  const [selectedDate, setSelectedDate] = useState(0);
 
   const selectedForecast = forecasts.find(
     (forecast) => forecast.date === selectedDate
@@ -27,28 +31,28 @@ const App = ({ location, forecasts }) => {
           forecasts={forecasts}
           onForecastSelect={handleForecastSelect}
         />
-        <ForecastDetails forecast={selectedForecast} />
+        {selectedForecast && <ForecastDetails forecast={selectedForecast} />}
       </div>
     </div>
   );
 };
 
-App.propTypes = {
-  forecasts: PropTypes.arrayOf(
-    PropTypes.shape({
-      date: PropTypes.number.isRequired,
-      description: PropTypes.string.isRequired,
-      icon: PropTypes.string.isRequired,
-      temperature: PropTypes.shape({
-        min: PropTypes.number,
-        max: PropTypes.number,
-      }),
-    })
-  ).isRequired,
-  location: PropTypes.shape({
-    city: PropTypes.string,
-    country: PropTypes.string,
-  }).isRequired,
-};
+// App.propTypes = {
+//   forecasts: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       date: PropTypes.number.isRequired,
+//       description: PropTypes.string.isRequired,
+//       icon: PropTypes.string.isRequired,
+//       temperature: PropTypes.shape({
+//         min: PropTypes.number,
+//         max: PropTypes.number,
+//       }),
+//     })
+//   ).isRequired,
+//   location: PropTypes.shape({
+//     city: PropTypes.string,
+//     country: PropTypes.string,
+//   }).isRequired,
+// };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { location, forecasts } from "./data/forecast.json";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App location={location} forecasts={forecasts} />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );

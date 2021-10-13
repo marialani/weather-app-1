dfd8a85743fc7513687a835053ad3d9106881194
Khalid Safy <khalid_safy88@hotmail.com>
2021-10-13 23:59:48 +0100

    Updated getForecast to have dynamic endpoint functionality

--- src/requests/getForecast.js
+++ src/requests/getForecast.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
-const getForecast = (setSelectedDate, setForecasts, setLocation) => {
-  const endpoint = "https://mcr-codes-weather-app.herokuapp.com/forecast";
+const getForecast = (
+  setSelectedDate,
+  setForecasts,
+  setLocation,
+  searchText
+) => {
+  let endpoint = "https://mcr-codes-weather-app.herokuapp.com/forecast";
+
+  if (searchText) {
+    endpoint += `?city=${searchText}`;
+  }
 
   axios.get(endpoint).then((response) => {
     setSelectedDate(response.data.forecasts[0].date);
